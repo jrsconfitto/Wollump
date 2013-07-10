94e0c02793e01550d41c30873750ba74968fc4ff
James R Sconfitto <jugglingnutcase@gmail.com>
2013-07-09 22:49:22 -0400

    Get basic rendering for wiki tags
    Also:
    * Replaced spaces with dashes for page requests
    * Refactored a bit

--- Wollump/WollumpModule.cs
+++ Wollump/WollumpModule.cs
@@ -3,37 +3,62 @@
     using LibGit2Sharp;
     using MarkdownSharp;
     using Nancy;
+    using Nancy.Helpers;
     using System;
     using System.IO;
     using System.Linq;
-
+    using System.Text.RegularExpressions;
     using Wollump.Models;
 
     public class WollumpModule : NancyModule
     {
-        private string[] indexPages = { "home", "readme", "index" };
-        private string[] validExtensions = { ".md", ".markdown", ".mkd" };
+        private string[] _indexPages = { "home", "readme", "index" };
+        private string[] _validExtensions = { ".md", ".markdown", ".mkd" };
+
+        private Repository _repo;
+        private Markdown _md;
 
         public WollumpModule(Repository repo, Markdown md)
         {
+            _repo = repo;
+            _md = md;
+
             Get["/"] = _ =>
             {
                 // Look for home/readme/index in the repository
-                var indexFile = repo.Head.Tip.Tree
+                var indexEntry = repo.Head.Tip.Tree
                     .Where(t => MatchesHomeFile(t.Name))
                     .Select(t => t.Target)
                     .FirstOrDefault();
 
-                if (indexFile != null)
+                if (indexEntry != null)
                 {
-                    var indexContent = repo.Lookup<Blob>(indexFile.Id).ContentAsUtf8();
-
-                    PageModel model = new PageModel() { Name = "Home", Content = md.Transform(indexContent) };
-                    return View["page", model];
+                    return View["page", ModelForBlobId(indexEntry.Id)];
                 }
                 else
                 {
                     return "There is no home to show. Maybe create it?";
+                }
+            };
+
+            Get["/page/{page}"] = parameters =>
+            {
+                string page = HttpUtility.UrlDecode(parameters.page).Replace(" ", "-");
+
+                var pageEntry = repo.Head.Tip.Tree
+                    .Where(t =>
+                        Path.GetFileNameWithoutExtension(t.Name).ToLowerInvariant() == page.ToLowerInvariant() &&
+                        HasRenderableExtension(t.Name))
+                    .Select(t => t.Target)
+                    .FirstOrDefault();
+
+                if (pageEntry != null)
+                {
+                    return View["page", ModelForBlobId(pageEntry.Id, page)];
+                }
+                else
+                {
+                    return page + " doesn't exist. Maybe create it?";
                 }
             };
 
@@ -46,38 +71,54 @@
                 return View["pages", validPages.ToArray()];
             };
 
-            Get["/page/{page}"] = parameters =>
+        }
+
+        private PageModel ModelForBlobId(ObjectId blobId, string name = "Home")
+        {
+            var content = _repo.Lookup<Blob>(blobId).ContentAsUtf8();
+
+            return new PageModel()
             {
-                string page = parameters.page;
-
-                var file = repo.Head.Tip.Tree
-                    .Where(t => 
-                        Path.GetFileNameWithoutExtension(t.Name).ToLowerInvariant() == page.ToLowerInvariant() &&
-                        HasRenderableExtension(t.Name))
-                    .Select(t => t.Target)
-                    .FirstOrDefault();
-
-                if (file != null)
-                {
-                    var content = repo.Lookup<Blob>(file.Id).ContentAsUtf8();
-                    return md.Transform(content);
-                }
-                else
-                {
-                    return page + " doesn't exist. Maybe create it?";
-                }
+                Name = name,
+                Content = RenderContent(content)
             };
+        }
+
+        private string RenderContent(string content)
+        {
+            return _md.Transform(ProcessTags(content));
         }
 
         private bool HasRenderableExtension(string filename)
         {
-            return validExtensions.Contains(Path.GetExtension(filename), StringComparer.InvariantCultureIgnoreCase);
+            return _validExtensions.Contains(Path.GetExtension(filename), StringComparer.InvariantCultureIgnoreCase);
         }
-    
+
         private bool MatchesHomeFile(string fileName)
         {
             string strippedName = Path.GetFileNameWithoutExtension(fileName);
-            return indexPages.Contains(strippedName, StringComparer.InvariantCultureIgnoreCase);
+            return _indexPages.Contains(strippedName, StringComparer.InvariantCultureIgnoreCase);
+        }
+
+        /// <summary>
+        /// Process the `[[link]]` tags real stoopid like!
+        /// </summary>
+        private string ProcessTags(string content)
+        {
+            // Thanks to Gollum for the Regex!
+            Regex tags = new Regex(@"(.?)\[\[(.+?)\]\]([^\[]?)");
+            MatchCollection matches = tags.Matches(content, 0);
+
+            foreach (Match match in matches)
+            {
+                string linkHref = HttpUtility.UrlEncode(match.Groups[2].Value);
+                string linkText = match.Groups[2].Value;
+
+                string replacement = string.Format(@"<a href=""/page/{0}"">{1}</a>", linkHref, linkText);
+                content = content.Replace(match.Value.Trim(), replacement);
+            }
+
+            return content;
         }
     }
 }
